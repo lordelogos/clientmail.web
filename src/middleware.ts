32d404f5824a367478546e95ec9efe15306ec940
lordelogos <paulehiks@gmail.com>
2023-08-06 09:04:47 +0100

    fix: middleware for clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { routes } from "./lib/routes";
-import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: [routes.home, routes.auth],
-  async afterAuth(auth, req) {
-    if (auth.isPublicRoute) {
-      return NextResponse.next();
-    }
-
-    const url = new URL(req.nextUrl.origin);
-
-    if (!auth.userId) {
-      url.pathname = routes.auth;
-      return NextResponse.redirect(url);
-    }
-  },
+  publicRoutes: [routes.home],
 });
 
 export const config = {
